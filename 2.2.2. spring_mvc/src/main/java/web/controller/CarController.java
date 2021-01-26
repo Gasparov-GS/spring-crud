013c2d056b1e975bf9d6a9bdbc467210f9c2caa5
Grigory Gasparov <gasparovapp@icloud.com>
2021-01-26 22:56:49 +0400

    2021-01-26 printWelcome

--- 2.2.2. spring_mvc/src/main/java/web/controller/CarController.java
+++ 2.2.2. spring_mvc/src/main/java/web/controller/CarController.java
@@ -1,13 +1,12 @@
 package web.controller;
 
-import org.springframework.web.bind.annotation.*;
-import web.model.Car;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import web.model.Car;
 import web.service.CarService;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -23,11 +22,16 @@ public class CarController {
 
     @GetMapping(value = "/cars")
     public String printWelcome(ModelMap model, HttpServletRequest request) {
-        int count = Integer.parseInt(request.getParameter("count"));
-        List<Car> cars = carService.getCars(count);
-        System.out.println(count);
+        List<Car> cars = null;
+
+        if (request.getQueryString() == null) {
+            cars = carService.getCars(5);
+        } else {
+            int count = Integer.parseInt(request.getParameter("count"));
+            cars = carService.getCars(count);
+        }
+
         model.addAttribute("forms", cars);
         return "car";
     }
-
 }
