babce6dea71216dbad29bb04f703286b66858ee1
Grigory Gasparov <gasparovapp@icloud.com>
2021-01-29 18:40:12 +0400

    2021-01-29 EntityManagerConfig

--- 2.2.2. spring_mvc/src/main/java/web/controller/CarController.java
+++ 2.2.2. spring_mvc/src/main/java/web/controller/CarController.java
@@ -3,6 +3,8 @@ package web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import web.model.Car;
 import web.service.CarService;
 
@@ -22,11 +24,20 @@ public class CarController {
 
     @GetMapping(value = "/cars")
     public String printWelcome(ModelMap model, HttpServletRequest request) {
-        List<Car> cars = request.getQueryString() == null
+        List<Car> cars = request.getParameter("count") == null
                         ? carService.getCars(5)
                         :carService.getCars(Integer.parseInt(request.getParameter("count")));
 
         model.addAttribute("forms", cars);
         return "car";
     }
+
+
+    @GetMapping("/user")
+    @ResponseBody
+    public String welcome() {
+        return new Car("d","d",3).toString();
+    }
+
+
 }
